9b337de64be65179f49edbb25c5d285575c419c3
Ricardo <rnogueradrum@gmail.com>
2021-07-24 10:55:38 +0200

    commit Suite Test

---
+++ src/test/java/com/example/demo/SuiteTest.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("com.example") 
+@SuiteDisplayName("Suite de testing") 
+class SuiteTest {
+
+	/**
+	 * El ejercicio solicita la cobertura de las clases indicadas
+	 * 
+	 * Suite de Test
+	 * 
+	 * General: (100%)
+	 * 
+	 * CarFacade
+	 * CarFactory
+	 * 
+	 * Services: (100%)
+	 * 
+	 * ElectricCarImpl
+	 * HybridCarImpl
+	 * GasCarServiceImpl
+	 */
+
+
+}
